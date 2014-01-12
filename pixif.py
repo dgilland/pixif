1265c5fe81cd282298748759fab37ee79e8408c8
Derrick Gilland <dgilland@gmail.com>
2014-01-12 00:37:00 -0500

    Major refactor.
    Handle images without EXIF by falling back to file modification date.

--- pixif.py
+++ pixif.py
@@ -7,242 +7,270 @@ import EXIF
 
 import os
 import shutil
-import datetime
+from datetime import datetime
 import ConfigParser
 
-# datetime tags to use for getting datetime of photo
-# NOTE: will try each tag until a valid datetime is extracted
-#  once valid will not look at the remaining tags
-EXIF_DATETIME_TAGS  = ( 'DateTimeOriginal', 'DateTimeDigitized', 'DateTime' )
+class PixifLogger(object):
+    def __init__(self, section, filename_out):
+        self.section = section
+        self.filename_out = filename_out
+        self.clear()
 
-# datetime format to attempt to extra from EXIF_DATETIME_TAG
-# NOTE: similar to EXIF_DATETIME_TAGS, will attempt to parse datetime using formats listed in order until valid
-EXIF_DATETIME_STRF  = ( '%Y:%m:%d %H:%M:%S', )
+    def append(self, text, image, dst):
+        self.logs.append('\t'.join([
+            datetime.today().isoformat(),
+            self.section,
+            text,
+            image.filename,
+            dst
+        ]))
 
-DATETIME_TUPLE_TAGS    = (
-    '%Y %m %d %H %M %S %w %j', ( 'Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'Weekday', 'Yearday' )
-)
+    def clear(self):
+        self.logs = []
 
-class PixifImage:
-    def __init__( self, filename, debug=False ):
-        self.debug  = debug
-        self.file   = open( filename, 'rb' )
-        self.exif   = EXIF.process_file( self.file )
-        if not self.exif:
-            raise Exception( 'no EXIF data' )
+    def write(self, filename_out=None):
+        if not self.logs:
+            return
 
-        self.filename   = filename
-        self.tags       = {
-            'Name':         os.path.basename( filename ),
-            'Extension':    os.path.splitext( filename )[1]
-        }
+        f_out = filename_out if filename_out else self.filename_out
 
-        self.get_tags()
-        self.get_datetime()
-        self.file.close()
+        try:
+            with open(f_out, 'a') as f:
+                for log in self.logs:
+                    try:
+                        f.write(str(log) + '\n')
+                    except IOError:
+                        pass
+        except IOError:
+            pass
 
-    def __repr__( self ):
-        return '<PixifImage at %s>' % ( self.filename )
+class PixifImage(object):
+    # Datetime tags to use for getting datetime of photo.
+    # Will try each tag until a valid datetime is extracted once valid will not look at the remaining tags.
+    EXIF_DATETIME_TAGS = ('DateTimeOriginal', 'DateTimeDigitized', 'DateTime')
 
-    def get_tags( self ):
-        tags    = self.exif.keys()
-        tags.sort()
-        for tag in tags:
-            if tag in ('JPEGThumbnail', 'TIFFThumbnail'):
-                # skip thumbnails
+    # Datetime format to attempt to extra from EXIF_DATETIME_TAG.
+    # Similar to EXIF_DATETIME_TAGS, will attempt to parse datetime using formats listed in order until valid.
+    EXIF_DATETIME_STRF = ('%Y:%m:%d %H:%M:%S',)
+
+    DATETIME_FORMAT = {
+        'tags': ('Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'Weekday', 'Yearday'),
+        'strf': ('%Y',   '%m',    '%d',  '%H',   '%M',     '%S',     '%w',      '%j')
+    }
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.datetime = None
+        self.tags = {}
+
+        with open(filename, 'rb') as f:
+            self.exif_data = EXIF.process_file(f)
+
+        self.set_file_tags()
+        self.set_exif_tags()
+        self.set_datetime_tags()
+
+    def __repr__(self):
+        return '<PixifImage at {0}>'.format(self.filename)
+
+    def __iter__(self):
+        return self.tags.iteritems()
+
+    def set_file_tags(self):
+        self.tags.update({
+            'Name': os.path.basename(self.filename),
+            'Extension': os.path.splitext(self.filename)[1]
+        })
+
+    def set_exif_tags(self):
+        for tag in sorted(self.exif_data.keys()):
+            # skip thumbnails
+            if 'thumbnail' in tag.lower():
                 continue
+
             try:
                 # format of tag is IFD name followed by tag name, e.g. 'EXIF DateTimeOriginal', 'Image Orientation'
                 # drop the IFD name and skip if duplicate
                 t = tag.split()[1]
-                if t not in self.tags:
-                    # use string value for tag value for use with filenames
-                    self.tags[t]  = str( self.exif[tag] )
+
+                # use string value for tag value for use with filenames
+                self.tags.setdefault(t, str(self.exif_data[tag]))
             except Exception:
                 # something went wrong but what can we do; tag value not usable
-                if self.debug:
-                    print 'PixifImage: (warning) could not get tag [%s] for [%s]' % ( tag, self.filename )
                 pass
 
-    def get_datetime( self ):
-        for t in [ e for e in EXIF_DATETIME_TAGS if e in self.tags ]:
-            for strf in EXIF_DATETIME_STRF:
-                try:
-                    date_str        = str( self.tags[t] )
-                    self.datetime   = datetime.datetime.strptime( date_str, strf )
-                except ValueError:
-                    # invalid date string for format
-                    if self.debug:
-                        print 'PixifImage: (warning) invalid date string [%s] using format [%s] for [%s]' % ( date_str, strf, self.filename )
-                    pass
-                except Exception,e:
-                    if self.debug:
-                        print 'PixifImage: (error) %s for [%s]' % ( e, self.filename )
-                    pass
+    def set_datetime_tags(self):
+        self.set_datetime()
 
         if self.datetime:
-            date_parts   = self.datetime.strftime( DATETIME_TUPLE_TAGS[0] ).split()
-            for i,dt_val in enumerate( date_parts ):
-                self.tags[ DATETIME_TUPLE_TAGS[1][i] ]   = dt_val
+            date_parts = self.datetime.strftime(' '.join(self.DATETIME_FORMAT['strf'])).split()
+            for i, value in enumerate(date_parts):
+                tag = self.DATETIME_FORMAT['tags'][i]
+                self.tags[tag] = value
 
-    def as_dict( self ):
-        return self.tags
+    def set_datetime(self):
+        if self.exif_data:
+            self.datetime = self.datetime_from_exif()
 
-class PixifCollection:
-    def __init__( self, src, dst, saveas, overwrite=True, logger=None, debug=False ):
-        self.src    = src
-        self.dst    = dst
-        self.saveas = saveas
-        self.overwrite  = overwrite
-        self.logger = logger
-        self.debug  = debug
-        self.images = None
-        self.collect()
+        if not self.exif_data or not self.datetime:
+            self.datetime = self.datetime_from_file()
 
-    # TODO: may not want to collect files only to re-loop through them in instructions
-    # perhaps overload collect() so that it functions as a generator if self.images is None
-    def collect( self ):
-        # files in source may not all contain EXIF data
-        potentials  = get_files( self.src )
-        self.images = files_to_pixif( potentials, debug=self.debug )
+    def datetime_from_exif(self):
+        dt = None
 
-        return self.images
+        for tag in self.EXIF_DATETIME_TAGS:
+            if tag not in self.tags:
+                continue
 
-    def instructions( self ):
-        for i in self.images:
-            yield ( i, os.path.join( self.dst, self.saveas.format( **i.as_dict() ) ) )
-
-    def copy( self ):
-        return self._operate( shutil.copy2 )
-
-    def move( self ):
-        return self._operate( shutil.move )
-
-    def _operate( self, operator ):
-        # TODO: need to add an overwrite option; currently files are overwritten
-        for image, dst_file in self.instructions():
-            if self.overwrite or not os.path.exists( dst_file ):
+            for strf in self.EXIF_DATETIME_STRF:
                 try:
-                    prepare_destination( dst_file )
+                    date_str = str(self.tags[tag])
+                    dt = datetime.strptime(date_str, strf)
+                    break
+                except Exception:
+                    pass
+
+        return dt
+
+    def datetime_from_file(self):
+        return datetime.fromtimestamp(os.path.getmtime(self.filename))
+
+
+class PixifCollection(object):
+    VALID_FILE_EXT = ('.jpg', '.jpeg', '.png')
+
+    def __init__(self, src, dst, saveas, method='move', overwrite=False, logger=None, **ignore):
+        self.src = src
+        self.dst = dst
+        self.saveas = saveas
+        self.method = method
+        self.overwrite = overwrite
+        self.logger = logger
+
+        self.set_images()
+
+    def execute(self):
+        if self.method == 'copy':
+            self.copy()
+        elif self.method == 'move':
+            self.move()
+
+    def copy(self):
+        return self._process(shutil.copy2)
+
+    def move(self):
+        return self._process(shutil.move)
+
+    def _process(self, operator):
+        for image in self.images:
+            log = None
+            dst_file = os.path.join(self.dst, self.saveas.format(**dict(image)))
+
+            if self.overwrite or not os.path.exists(dst_file):
+                head, tail = os.path.split(dst_file)
+
+                try:
+                    os.makedirs(head)
                 except OSError:
                     # destination root already exists
                     pass
 
-                log = ''
                 try:
-                    operator( image.filename, dst_file )
-                    log = '(success) processed image using %s' % operator
-                except OSError,e:
-                    log = e
+                    operator(image.filename, dst_file)
+                    log = '(success) processed image using {0}'.format(operator)
+                except OSError as e:
+                    log = str(e)
             else:
                 log = '(warning) could not process image because file already exists'
 
-            if log:
-                self.logger.append( log, image, dst_file )
+            if log and self.logger:
+                self.logger.append(log, image, dst_file)
 
-class PixifLogger:
-    def __init__( self, section, filename_out ):
-        self.section        = section
-        self.filename_out   = filename_out
-        self.clear()
+    def set_images(self):
+        self.images = []
 
-    def append( self, text, image, dst ):
-        self.logs.append( PixifLogEntry( text, image, dst, self.section ) )
-
-    def clear( self ):
-        self.logs   = []
-
-    def write( self, filename_out=None ):
-        if self.logs:
-            f_out   = filename_out if filename_out else self.filename_out
-            try:
-                f_out   = open( f_out, 'ab' )
-            except IOError:
-                pass
-            else:
-                for l in self.logs:
+        for root, dirs, filenames in os.walk(self.src):
+            for filename in filenames:
+                if os.path.splitext(filename)[1].lower() in self.VALID_FILE_EXT:
                     try:
-                        f_out.write( str(l) + '\n' )
-                    except IOError:
-                        pass
+                        self.images.append(PixifImage(os.path.join(root, filename)))
+                    except Exception as e:
+                        print e
 
-                f_out.close()
-
-class PixifLogEntry:
-    def __init__( self, text, image, dst, section ):
-        self.text       = text
-        self.image      = image
-        self.dst        = dst
-        self.section    = section
-        self.datetime   = datetime.datetime.today()
-
-    def __repr__( self ):
-        return '\t'.join( [ self.datetime.isoformat(' '), self.section, self.text, self.image.filename, self.dst ] )
-
-def get_files( src ):
-    files   = []
-    for root, dirs, filenames in os.walk( src ):
-        files += [ os.path.join( root, f ) for f in filenames ]
-    return files
-
-def files_to_pixif( files, debug=False ):
-    pixifs  = []
-    for f in files:
-        try:
-            pixifs.append( PixifImage( f, debug=debug ) )
-        except Exception,e:
-            if debug:
-                print 'files_to_pixif: (error) %s for [%s]' % ( e, f )
-            continue
-
-    return pixifs
-
-def prepare_destination( filename ):
-    head,tail   = os.path.split( filename )
-    os.makedirs( head )
-
-
-def read_config_file( filename, booleans ):
-    config  = ConfigParser.RawConfigParser()
-    config.read( filename )
-
-    config_dict = dict()
-    for s in config.sections():
-        config_dict[s]  = dict()
-
-        for name,_ in config.items(s):
-            if config.has_option( s, name ):
-                if name in booleans:
-                    value   = config.getboolean( s, name )
-                else:
-                    value   = config.get( s, name )
-
-            config_dict[s][name]    = value
-
-    return config_dict
-
-def read_config_opts( opts, booleans ):
-    config  = dict( section=dict() )
-    for o,opt in opts.iteritems():
-        new_key = o.replace( '--', '' )
-
-        if new_key in booleans:
-            config['section'][new_key] = True
-        else:
-            config['section'][new_key] = opt
-
-    return config
-
-def config_merge_defaults( config ):
-    defaults    = {
-        'method':       'copy',
-        'log':          False,
-        'overwrite':    False,
-        'enabled':      True
+class PixifConfig(dict):
+    opts_map = {
+        '-s': 'src',
+        '-d': 'dst',
+        '-a': 'saveas',
+        '-m': 'method',
+        '-l': 'log',
+        '-o': 'overwrite',
     }
 
-    for c in config:
-        config[c]   = dict( defaults.items() + config[c].items() )
+    flags = ['log', 'overwrite', 'enabled']
+
+    defaults = {
+        'method': 'copy',
+        'log': False,
+        'overwrite': False,
+        'enabled': True
+    }
+
+    def __init__(self, filename=None, opts=None):
+        self.filename = filename
+        self.opts = opts
+
+        if filename:
+            # Assume that first element is config file name.
+            self.from_file(filename)
+        elif opts:
+            self.from_opts(opts)
+
+    def from_file(self, filename):
+        self.clear()
+
+        config = ConfigParser.RawConfigParser()
+        config.read(filename)
+
+        for s in config.sections():
+            self[s] = self.defaults.copy()
+
+            for name,_ in config.items(s):
+                if config.has_option(s, name):
+                    if name in self.flags:
+                        value = config.getboolean(s, name)
+                    else:
+                        value = config.get(s, name)
+
+                self[s][name] = value
+
+    def from_opts(opts):
+        self['section'] = self.defaults.copy()
+
+        opts_dict = dict(opts)
+
+        for o, opt in opts_dict.iteritems():
+            if o in self.opts_map:
+                key = self.opts_map[o]
+            else:
+                key = o.replace('--', '')
+
+            self['section'][key] = True if key in self.flags else opt
+
+def main(config_filename, opts):
+    config = PixifConfig(filename=config_filename, opts=opts)
+    logger_file = os.path.join(os.path.split(config_filename)[0], 'pixif.log')
+
+    for c, cfg in config.iteritems():
+        if not cfg['enabled']:
+            continue
+
+        logger = PixifLogger(c, logger_file)
+
+        collection = PixifCollection(logger=logger, **cfg)
+        collection.execute()
+
+        logger.write()
 
 if __name__ == '__main__':
 
@@ -250,69 +278,14 @@ if __name__ == '__main__':
     import getopt
 
     try:
-        opts, args  = getopt.getopt( sys.argv[1:], 's:d:a:m:lo', [ 'src=', 'dst=', 'saveas=', 'method=', 'log', 'overwrite' ] )
-        booleans    = [ 'log', 'overwrite', 'enabled' ]
-
-        if args:
-            config_file = args[0]
-            config  = read_config_file( config_file, booleans )
-        else:
-            config_file = ''
-            opt_map = {
-                '-s':   'src',
-                '-d':   'dst',
-                '-a':   'saveas',
-                '-m':   'method',
-                '-l':   'log',
-                '-o':   'overwrite',
-            }
-
-            opts_dict   = dict( opts )
-            opts        = dict()
-            for o,opt in opts_dict.iteritems():
-                if o in opt_map:
-                    opts[ opt_map[o] ]  = opt
-
-            config  = read_config_opts( opts, booleans )
-
-        config_merge_defaults( config )
-
-        required    = set( [ 'src', 'dst', 'saveas', 'method' ] )
-        opts_valid  = True
-        for c,conf in config.iteritems():
-            if len( required - set(conf.keys()) ) > 0:
-                opts_valid  = False
-                break
-
-        if not opts_valid:
-            print 'error:', 'missing required options'
-            sys.exit()
-
+        opts, unparsed = getopt.getopt(
+            sys.argv[1:],
+            's:d:a:m:lo',
+            ['src=', 'dst=', 'saveas=', 'method=', 'log', 'overwrite']
+        )
     except getopt.GetoptError:
-        print 'error:', 'incorrect usage'
+        print 'ERROR: Incorrect usage'
     else:
-        logger_dir,_    = os.path.split( config_file )
-        logger_file     = os.path.join( logger_dir, 'pixif.log' )
-
-        for c,cfg in config.iteritems():
-            if not cfg['enabled']:
-                continue
-
-            logger  = PixifLogger( c, logger_file )
-
-            try:
-                pixif_c = PixifCollection( cfg['src'], cfg['dst'], cfg['saveas'], cfg['overwrite'], logger=logger )
-
-                if cfg['method'] == 'copy':
-                    pixif_c.copy()
-                elif cfg['method'] == 'move':
-                    pixif_c.move()
-
-            except Exception,e:
-                print 'error:', e
-
-            logger.write()
-            logger.clear()
-
-    sys.exit()
+        config_filename = unparsed[0] if unparsed else ''
+        main(config_filename, opts)
 
